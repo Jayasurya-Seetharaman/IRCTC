6a7f94dacd6b7d85769e3359fc945b988d4a4818
Jayasurya-Seetharaman <jayasuryas06@gmail.com>
2018-09-21 04:38:37 +0530

    Uploading

---
+++ jq.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+$(document).ready(function(){
+   $("button").click(function(){
+        $("#div1").load("demo_test.txt");
+    });
+	
+	$("#bt1").click(function(){
+        //$("#div2").load("demo_test.html table");
+		alert("hello");
+    });
+});
